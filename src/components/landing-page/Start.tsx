b79aa356334d3991c2be443a8c206274c78c34d1
Bohdan Bilovodskyi <bilovodskyi@gmail.com>
2025-04-27 12:37:00 -0700

    Add third and fourth timeline animations, second scene

--- src/components/landing-page/Start.tsx
+++ src/components/landing-page/Start.tsx
@@ -22,6 +22,15 @@ export default function StartPage() {
     const stockCardFiveRef = useRef<HTMLDivElement | null>(null);
     const cardsContainerRef = useRef<HTMLDivElement | null>(null);
     const titleContainerRef = useRef<HTMLDivElement | null>(null);
+    const mainSectionRef = useRef<HTMLDivElement | null>(null);
+
+    const elOneSecondSceneRef = useRef<HTMLDivElement | null>(null);
+    const elTwoSecondSceneRef = useRef<HTMLDivElement | null>(null);
+    const elThreeSecondSceneRef = useRef<HTMLDivElement | null>(null);
+    const elFourSecondSceneRef = useRef<HTMLDivElement | null>(null);
+    const elFiveSecondSceneRef = useRef<HTMLDivElement | null>(null);
+    const elSixSecondSceneRef = useRef<HTMLDivElement | null>(null);
+    const elSevenSecondSceneRef = useRef<HTMLDivElement | null>(null);
 
     useGSAP(() => {
         if (
@@ -33,7 +42,15 @@ export default function StartPage() {
             !stockCardFourRef.current ||
             !stockCardFiveRef.current ||
             !cardsContainerRef.current ||
-            !titleContainerRef.current
+            !titleContainerRef.current ||
+            !mainSectionRef.current ||
+            !elOneSecondSceneRef.current ||
+            !elTwoSecondSceneRef.current ||
+            !elThreeSecondSceneRef.current ||
+            !elFourSecondSceneRef.current ||
+            !elFiveSecondSceneRef.current ||
+            !elSixSecondSceneRef.current ||
+            !elSevenSecondSceneRef.current
         )
             return;
         const initialTimeline = gsap.timeline({
@@ -61,6 +78,39 @@ export default function StartPage() {
             },
         });
 
+        const thirdAnimationTimeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: "#section-three",
+                start: "top bottom",
+                end: "+=150px",
+                scrub: true,
+            },
+        });
+        const forthAnimationTimeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: "#section-three",
+                start: "top bottom-=150px",
+                end: "top center",
+                scrub: true,
+            },
+        });
+        const fifthAnimationTimeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: "#section-three",
+                start: "top center",
+                end: "top top",
+                scrub: true,
+            },
+        });
+        const sixthAnimationTimeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: "#section-four",
+                start: "top bottom",
+                end: "bottom top",
+                scrub: true,
+            },
+        });
+
         const cards = [
             stockCardOneRef.current,
             stockCardTwoRef.current,
@@ -69,39 +119,50 @@ export default function StartPage() {
             stockCardFiveRef.current,
         ];
 
+        const secondSceneElements = [
+            elOneSecondSceneRef.current,
+            elTwoSecondSceneRef.current,
+            elThreeSecondSceneRef.current,
+            elFourSecondSceneRef.current,
+            elFiveSecondSceneRef.current,
+            elSixSecondSceneRef.current,
+            elSevenSecondSceneRef.current,
+        ];
+
         const fadeOut = [mainTitleRef.current, scrollToBottomButtonRef.current];
 
         const delays = [0.5, 0.3, 0.6, 0.4, 0.2];
 
-        initialTimeline.from(
-            mainTitleRef.current.children,
-            {
-                y: 100,
-                opacity: 0,
-                stagger: 0.1,
-            },
-            0
-        );
-        initialTimeline.from(
-            scrollToBottomButtonRef.current.children,
-            {
-                y: 50,
-                delay: 0.2,
-                opacity: 0,
-                stagger: 0.1,
-            },
-            0
-        );
-        initialTimeline.from(
-            cards,
-            {
-                y: 350,
-                duration: 1,
-                ease: "power4.out",
-                delay: (i) => delays[i],
-            },
-            0
-        );
+        initialTimeline
+            .from(
+                mainTitleRef.current.children,
+                {
+                    y: 100,
+                    opacity: 0,
+                    stagger: 0.1,
+                },
+                0
+            )
+            .from(
+                scrollToBottomButtonRef.current.children,
+                {
+                    y: 50,
+                    delay: 0.2,
+                    opacity: 0,
+                    stagger: 0.1,
+                },
+                0
+            )
+            .from(
+                cards,
+                {
+                    y: 350,
+                    duration: 1,
+                    ease: "power4.out",
+                    delay: (i) => delays[i],
+                },
+                0
+            );
 
         firstAnimationTimeline
             .to(
@@ -126,32 +187,94 @@ export default function StartPage() {
 
         secondAnimationTimeline
             .to(
-                cardsContainerRef.current,
+                cards,
                 {
-                    scale: 0.3,
-                    duration: 0.5,
+                    y: -250,
+                    width: "70px",
+                    height: "70px",
+                    duration: 0.7,
+                },
+                0
+            )
+            .to(mainSectionRef.current, {
+                backgroundColor: "var(--foreground)",
+            });
 
-                    ease: "power2.out",
+        thirdAnimationTimeline.to(
+            stockCardOneRef.current,
+            {
+                y: -350,
+            },
+            0
+        );
+        forthAnimationTimeline
+            .to(
+                stockCardOneRef.current,
+                {
+                    x: "+=400",
                 },
                 0
             )
             .to(
-                cards,
+                stockCardTwoRef.current,
                 {
-                    y: -1200,
-                    duration: 0.7,
-                    // ease: "power2.in",
-                    // ease: "power2.out",
+                    x: -150,
+                },
+                0
+            )
+            .to(
+                [
+                    stockCardThreeRef.current,
+                    stockCardFourRef.current,
+                    stockCardFiveRef.current,
+                ],
+                {
+                    y: "+=100",
                 },
                 0
             );
+
+        fifthAnimationTimeline
+            .to(
+                [stockCardFourRef.current, stockCardFiveRef.current],
+                {
+                    y: "+=100",
+                },
+                0
+            )
+            .to(
+                stockCardThreeRef.current,
+
+                {
+                    x: "-=50",
+                },
+                0
+            )
+            .to(
+                [stockCardFourRef.current, stockCardFiveRef.current],
+                {
+                    x: "+=200",
+                },
+                1
+            );
+
+        sixthAnimationTimeline.to(
+            secondSceneElements,
+            {
+                opacity: 1,
+                stagger: 0.02,
+            },
+            0
+        );
     });
     return (
         <>
-            <section className="md:h-screen fixed">
+            <section
+                ref={mainSectionRef}
+                className="fixed top-0 left-0 right-0 bottom-0 bg-background">
                 <div
                     ref={titleContainerRef}
-                    className="h-2/3 flex items-center justify-end flex-col gap-12 pb-24">
+                    className="h-2/3 flex items-center justify-end flex-col gap-12 pb-24 w-full">
                     <div
                         ref={mainTitleRef}
                         className="font-poppins text-[8rem]">
@@ -175,45 +298,85 @@ export default function StartPage() {
                 </div>
                 <div
                     ref={cardsContainerRef}
-                    className="h-1/3 flex px-4 py-2 justify-center gap-2">
+                    className="h-1/3 flex px-4 py-2 justify-center w-full gap-2">
                     <div
                         ref={stockCardOneRef}
-                        className="h-full aspect-square flex-none bg-[#478bff] rounded-full flex-center">
-                        <RiMetaFill className="text-background text-[14vw]" />
+                        className="h-full aspect-square flex-none bg-[#478bff] rounded-full flex-center border border-background">
+                        <p
+                            ref={elThreeSecondSceneRef}
+                            className="absolute -left-[660px] text-black font-poppins !text-[5rem] whitespace-nowrap opacity-0">
+                            Your AI Trading
+                        </p>
+                        <RiMetaFill className="absolute inset-0 m-auto w-3/5 h-3/5 text-background" />
+                        <p
+                            ref={elOneSecondSceneRef}
+                            className="absolute -right-[180px] text-black font-poppins !text-[5rem] opacity-0">
+                            Hub
+                        </p>
                     </div>
                     <div
                         ref={stockCardTwoRef}
-                        className="h-full aspect-square flex-none bg-[#ffc412] rounded-4xl flex-center">
-                        <IoLogoApple className="text-background text-[14vw]" />
+                        className="h-full aspect-square flex-none bg-[#ffc412] rounded-[10%] flex-center border border-background">
+                        <p
+                            ref={elTwoSecondSceneRef}
+                            className="absolute -left-[124px] text-black font-poppins !text-[5rem] whitespace-nowrap opacity-0">
+                            All
+                        </p>
+                        <IoLogoApple className="absolute inset-0 m-auto w-3/5 h-3/5 text-background" />
+                        <p
+                            ref={elFourSecondSceneRef}
+                            className="absolute -right-[580px] text-black font-poppins !text-[5rem] whitespace-nowrap opacity-0">
+                            Trading Tools
+                        </p>
                     </div>
                     <div ref={stockCardThreeRef} className="relative h-full">
-                        <svg viewBox="0 0 95 83" className="h-full" fill="none">
+                        <svg
+                            viewBox="0 0 95 83"
+                            className="h-full "
+                            fill="none">
                             <path
                                 d="M1.09009 9.13254C1.09009 4.65077 4.72328 1.01758 9.20505 1.01758L66.1868 1.01759C69.0848 1.01759 71.7628 2.563 73.2129 5.07211L91.9285 37.4571C93.3804 39.9694 93.3804 43.0657 91.9285 45.578L73.2129 77.9631C71.7628 80.4722 69.0848 82.0176 66.1868 82.0176H9.20505C4.72328 82.0176 1.09009 78.3844 1.09009 73.9026V9.13254Z"
                                 fill="#00a652"
+                                stroke="#141414"
                                 data-v-13822dcf=""></path>
                         </svg>
-
-                        <div className="absolute inset-0 flex items-center justify-center mr-12">
-                            <IoLogoAmazon className="text-background text-[14vw]" />
+                        <p
+                            ref={elSevenSecondSceneRef}
+                            className="absolute -left-[434px] -top-6 text-black font-poppins !text-[5rem] whitespace-nowrap opacity-0">
+                            You Might
+                        </p>
+                        <div className="absolute inset-0 flex items-center justify-center mr-[15%]">
+                            <IoLogoAmazon className="absolute inset-0 m-auto w-3/5 h-3/5 text-background" />
                         </div>
+                        <p
+                            ref={elFiveSecondSceneRef}
+                            className="absolute -right-[524px] -top-6 text-black font-poppins !text-[5rem] whitespace-nowrap opacity-0">
+                            Need To Win
+                        </p>
                     </div>
 
                     <div
                         ref={stockCardFourRef}
-                        className="h-full aspect-square flex-none bg-[#7a78ff] rounded-4xl flex-center">
-                        <RiNetflixFill className="text-background text-[12vw]" />
+                        className="h-full aspect-square flex-none bg-[#7a78ff] rounded-[10%] flex-center border border-background">
+                        <p
+                            ref={elSixSecondSceneRef}
+                            className="absolute -left-[646px] text-black font-poppins !text-[5rem] whitespace-nowrap opacity-0">
+                            You Next Trade
+                        </p>
+
+                        <RiNetflixFill className="absolute inset-0 m-auto w-3/5 h-3/5 text-background" />
                     </div>
                     <div
                         ref={stockCardFiveRef}
-                        className="h-full aspect-square flex-none bg-[#ff6d38] rounded-full flex-center">
-                        <IoLogoGoogle className="text-background text-[14vw]" />
+                        className="h-full aspect-square flex-none bg-[#ff6d38] rounded-full flex-center border border-background">
+                        <IoLogoGoogle className="absolute inset-0 m-auto w-3/5 h-3/5 text-background" />
                     </div>
                 </div>
             </section>
             <section id="section-one" className="h-screen "></section>
             <section id="section-two" className="h-screen "></section>
             <section id="section-three" className="h-screen "></section>
+            <section id="section-four" className="h-screen "></section>
         </>
     );
 }
